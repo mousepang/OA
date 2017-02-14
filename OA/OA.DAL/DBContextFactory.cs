80d6abf1522df1ceb976dd5a8048de898661bf73
freedom <freedom@DESKTOP-BMSAFSV>
2017-02-14 13:22:05 +0800

    2

---
+++ OA/OA.DAL/DBContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using OA.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.DAL
+{
+   public class DBContextFactory
+    {
+        /// <summary>
+        /// 负责创建Ef，保证线程内唯一
+        /// </summary>
+        /// <returns></returns>
+        public static DbContext CreateContext()
+        {
+            DbContext dbContext = (DbContext)CallContext.GetData("dbContext");
+            if(dbContext==null)
+            {
+                dbContext = new OAEntities();
+                CallContext.SetData("dbContext",dbContext);
+            }
+            return dbContext;
+        }
+    }
+}
